b755671ebfbcd9b861ae5f820cff848162116845
Tom Lauerman <tlauerman@google.com>
2020-07-07 16:49:12 +0000

    Fix all major pylint warnings in ml/main.py
    The `redefined-outer-name` warning is not addressed in this commit.
    `redefined-outer-name` will be fixed in a subsequent commit which will
    separate the helper functions from the main execution of the script.

--- ml/main.py
+++ ml/main.py
@@ -4,19 +4,19 @@ Note: this file is best run as an interactive Jupyter notebook
 """
 
 # %% define imports and functions
+import numpy as np
+import pandas as pd
 import tensorflow as tf
 from tensorflow import keras
-import pandas as pd
-import numpy as np
 
-fdc_data_dir = '../data/fdc/'
+FDC_DATA_DIR = '../data/fdc/'
 
 
 def load_calorie_data():
   """Load calorie_data from relevant csv files in FDC datatset."""
-  food = pd.read_csv(fdc_data_dir + 'food.csv').set_index('fdc_id')
-  nutrient = pd.read_csv(fdc_data_dir + 'nutrient.csv')
-  food_nutrient = pd.read_csv(fdc_data_dir + 'food_nutrient.csv')
+  food = pd.read_csv(FDC_DATA_DIR + 'food.csv').set_index('fdc_id')
+  nutrient = pd.read_csv(FDC_DATA_DIR + 'nutrient.csv')
+  food_nutrient = pd.read_csv(FDC_DATA_DIR + 'food_nutrient.csv')
 
   # combine food & nutrient data
   combined = food.join(food_nutrient.set_index('fdc_id'))
@@ -35,18 +35,17 @@ def load_calorie_data():
 
 def add_calorie_labels(calorie_data, low_calorie_threshold,
                        high_calorie_threshold):
-  """Set 'calorie_label' for each row in the dataset to reflect which class of calorie that dish falls under"""
+  """Set 'calorie_label' for each row in the dataset to reflect which class of calorie that dish falls under."""
 
   def label_row(calorie_data_row):
     if calorie_data_row['amount'] < low_calorie_threshold:
-      return [1, 0, 0] # low calorie
+      return [1, 0, 0]  # low calorie
     elif calorie_data_row['amount'] > high_calorie_threshold:
-      return [0, 0, 1] # high calorie
+      return [0, 0, 1]  # high calorie
     else:
-      return [0, 1, 0] # avg calorie
+      return [0, 1, 0]  # avg calorie
 
-  calorie_data['calorie_label'] = calorie_data.apply(
-      lambda row: label_row(row), axis=1)
+  calorie_data['calorie_label'] = calorie_data.apply(label_row, axis=1)
 
   return calorie_data
 
@@ -69,28 +68,31 @@ def max_sequence_length(series):
 
   return max_len
 
+
 def add_input_labels(calorie_data, vocab_size, max_corpus_length):
   """Set 'input' for each row in the dataset to reflect the encoded input of the text."""
 
   def input_for_row(row):
     encoded = keras.preprocessing.text.one_hot(row['description'], vocab_size)
-    
+
     while len(encoded) < max_corpus_length:
       encoded.append(0)
 
     return encoded
 
-  calorie_data['input'] = calorie_data.apply(lambda row: input_for_row(row), axis=1)
+  calorie_data['input'] = calorie_data.apply(input_for_row, axis=1)
   return calorie_data
 
 
 # %% read data from disk
 print(f'Tensorflow version {tf.__version__}')
 calorie_data = load_calorie_data()
-add_calorie_labels(calorie_data, low_calorie_threshold=100, high_calorie_threshold=500)
+add_calorie_labels(
+    calorie_data, low_calorie_threshold=100, high_calorie_threshold=500)
 
 # %% add encode 'description' into a new 'input' column in calorie_data
-calorie_data['description'] = calorie_data['description'].str.replace(',', '').str.lower()
+calorie_data['description'] = calorie_data['description'].str.replace(
+    ',', '').str.lower()
 corpus = calorie_data['description']
 vocab_size = num_unique_words(corpus)
 tokenized_corpus = corpus.map(lambda desc: desc.split(' '))
@@ -99,20 +101,26 @@ calorie_data = add_input_labels(calorie_data, vocab_size, max_corpus_length)
 
 # %% create model
 model = keras.Sequential([
-  keras.layers.Embedding(vocab_size, int(vocab_size**(1/4)), input_length=max_corpus_length),
-  keras.layers.Flatten(),
-  keras.layers.Dense(3, activation='softmax'),
+    keras.layers.Embedding(
+        vocab_size, int(vocab_size**(1 / 4)), input_length=max_corpus_length),
+    keras.layers.Flatten(),
+    keras.layers.Dense(3, activation='softmax'),
 ])
 
 model.compile(
-  optimizer='adam',
-  loss='categorical_crossentropy',
-  metrics=['categorical_accuracy', keras.metrics.Precision(), keras.metrics.Recall()]
-)
+    optimizer='adam',
+    loss='categorical_crossentropy',
+    metrics=[
+        'categorical_accuracy',
+        keras.metrics.Precision(),
+        keras.metrics.Recall()
+    ])
 
 # %% model stats
 model.summary()
-model._layers = [layer for layer in model._layers if not isinstance(layer, dict)] # workaround for bug in keras.util.plot_model
+model._layers = [  # workaround for bug in keras.util.plot_model pylint: disable=protected-access
+    layer for layer in model._layers if not isinstance(layer, dict)  # pylint: disable=protected-access
+]
 keras.utils.plot_model(model, show_layer_names=False, show_shapes=True)
 
 # %% split dataset
@@ -121,30 +129,25 @@ validation_frac = 0.2
 test_frac = 0.2
 
 train_set, validation_set, test_set = np.split(
-  calorie_data.sample(frac=1), # shuffle data
-  [
-    int(train_frac * len(calorie_data)),
-    int((train_frac + validation_frac) * len(calorie_data)),
-  ]
-)
+    calorie_data.sample(frac=1),  # shuffle data
+    [
+        int(train_frac * len(calorie_data)),
+        int((train_frac + validation_frac) * len(calorie_data)),
+    ])
 
 # %% train model
 history = model.fit(
-  # keras.utils.to_categorical(train_set['description'], num_classes=vocab_size),
-  np.stack(train_set['input']),
-  np.stack(train_set['calorie_label']),
-  validation_data=(
-    np.stack(validation_set['input']),
-    np.stack(validation_set['calorie_label'])
-  ),
-  callbacks=[keras.callbacks.TensorBoard()],
+    np.stack(train_set['input']),
+    np.stack(train_set['calorie_label']),
+    validation_data=(np.stack(validation_set['input']),
+                     np.stack(validation_set['calorie_label'])),
+    callbacks=[keras.callbacks.TensorBoard()],
 )
 
 # %% evaluate model
 results = model.evaluate(
-  # keras.utils.to_categorical(test_set['description'], num_classes=vocab_size),
-  np.stack(test_set['input']),
-  np.stack(test_set['calorie_label']),
+    np.stack(test_set['input']),
+    np.stack(test_set['calorie_label']),
 )
 
 print(results)
