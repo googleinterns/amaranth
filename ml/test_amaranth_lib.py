a62b05890b59521f1ebc8442b935b5f2d007f19c
Tom Lauerman <tlauerman@google.com>
2020-07-09 19:50:51 +0000

    Add test messages to combine_dataframes tests

--- ml/test_amaranth_lib.py
+++ ml/test_amaranth_lib.py
@@ -9,7 +9,9 @@ import pandas as pd
 class TestAmaranthHelpers(unittest.TestCase):
 
   def test_combine_dataframes(self):
-    self.assertTrue(amaranth.combine_dataframes('').equals(pd.DataFrame()))
+    self.assertTrue(
+        amaranth.combine_dataframes('').equals(pd.DataFrame()),
+        'Combining no DataFrames yields an empty DataFrame')
     self.assertTrue(
         amaranth.combine_dataframes(
             'id', pd.DataFrame(data={
@@ -19,7 +21,8 @@ class TestAmaranthHelpers(unittest.TestCase):
                 pd.DataFrame(data={
                     'id': [1, 2, 3],
                     'val': [4, 5, 6],
-                }).set_index('id')))
+                }).set_index('id')),
+        'Combining one DataFrame yields the same DataFrame')
     self.assertTrue(
         amaranth.combine_dataframes(
             'id', pd.DataFrame(data={
@@ -35,8 +38,13 @@ class TestAmaranthHelpers(unittest.TestCase):
                         'id': [1, 2, 3],
                         'val': [4, 5, 6],
                         'str': ['four', 'five', 'six'],
-                    }).set_index('id')))
-    with self.assertRaises(KeyError):
+                    }).set_index('id')),
+        'Combining two DataFrames yields one DataFrame with the properties of both'
+    )
+    with self.assertRaises(
+        KeyError,
+        msg='Combining DataFrame(s) with an index that isn\'t present results in a KeyError'
+    ):
       amaranth.combine_dataframes(
           'wrong_id', pd.DataFrame(data={
               'id': [1, 2, 3],
