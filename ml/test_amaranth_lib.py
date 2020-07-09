07bdbb6a36fc6345963c8aec31f1c2526ee60118
Tom Lauerman <tlauerman@google.com>
2020-07-09 15:38:26 +0000

    Add test method stubs in ml/test_amaranth_lib.py

---
+++ ml/test_amaranth_lib.py
@@ -0,0 +1,29 @@
+# Lint as: python3
+"""These tests ensure correctness for the helper functions in amaranth_lib."""
+
+import unittest
+
+
+class TestAmaranthHelpers(unittest.TestCase):
+
+  def test_load_calorie_data(self):
+    raise NotImplementedError
+
+  def test_clean_data(self):
+    raise NotImplementedError
+
+  def test_add_calorie_labels(self):
+    raise NotImplementedError
+
+  def test_num_unique_words(self):
+    raise NotImplementedError
+
+  def test_max_sequence_length(self):
+    raise NotImplementedError
+
+  def test_add_input_labels(self):
+    raise NotImplementedError
+
+
+if __name__ == '__main__':
+  unittest.main()
