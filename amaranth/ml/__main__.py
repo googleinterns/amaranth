80ee270863dc172ca9f86522320d1905eb59d486
Tom Lauerman <tlauerman@google.com>
2020-07-14 19:19:12 +0000

    Update docstring for amaranth.ml.__main__

--- amaranth/ml/__main__.py
+++ amaranth/ml/__main__.py
@@ -1,5 +1,8 @@
 # Lint as: python3
-"""This script is used to run the amaranth.ml module."""
+"""This script is used to run the amaranth.ml module as an executable.
+
+For now, this script just delegates it's main to amaranth.ml.train
+"""
 
 from amaranth.ml import train
 
