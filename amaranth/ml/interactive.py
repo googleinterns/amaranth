ba3e51226a87b03d14da5e92bc595e8b6b962dab
Tom Lauerman <tlauerman@google.com>
2020-07-17 19:21:11 +0000

    Create interactive.py file
    This is where all the model interactivity code will live.

---
+++ amaranth/ml/interactive.py
@@ -0,0 +1,14 @@
+# Lint as: python3
+"""This script runs the ML model in an interactive mode.
+
+Here, you can type in any arbitrary text and the model will attempt to classify
+it as a low, average, or high-calorie dish.
+"""
+
+
+def main():
+  print('Hello world!')
+
+
+if __name__ == '__main__':
+  main()
