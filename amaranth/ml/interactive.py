7abfedc110900061c2241c90a1d54576220dc029
Tom Lauerman <tlauerman@google.com>
2020-07-17 20:48:12 +0000

    Successfully load ML model from file

--- amaranth/ml/interactive.py
+++ amaranth/ml/interactive.py
@@ -5,9 +5,23 @@ Here, you can type in any arbitrary text and the model will attempt to classify
 it as a low, average, or high-calorie dish.
 """
 
+import os
+
+import tensorflow_addons as tfa
+from tensorflow import keras
+
+# Project resources directory
+RESOURCES_DIR = '../resources/'
+
 
 def main():
-  print('Hello world!')
+  current_dir = os.path.dirname(__file__)
+
+  model = keras.models.load_model(
+      os.path.join(current_dir, RESOURCES_DIR, 'model'),
+      custom_objects={'F1Score': tfa.metrics.F1Score(num_classes=3)})
+
+  print(model.summary())
 
 
 if __name__ == '__main__':
