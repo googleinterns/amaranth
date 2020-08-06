b8d12a6df026ab35db7f41a681812a3e6f448ee8
Tom Lauerman <tlauerman@google.com>
2020-08-06 14:33:34 +0000

    Add ML model loading in Chrome extension

--- amaranth-chrome-ext/content.js
+++ amaranth-chrome-ext/content.js
@@ -1,13 +1,18 @@
+const mlModelPath = chrome.runtime.getURL('model/model.json');
 const tokenizerPath = chrome.runtime.getURL('tokenizer.json');
 
 async function main() {
   // Load tokenizer dictionary
-  const tokenizerResponse = await fetch(tokenizerPath);
-  const tokenizerObj = await tokenizerResponse.json();
+  const response = await fetch(tokenizerPath);
+  const tokenizerObj = await response.json();
   const tokenizer = new Map(Object.entries(tokenizerObj));
 
   console.log(tokenizer.entries());
-  console.log(tokenizer.get('bread'));
+  console.log(`Tokenizer dict size: ${tokenizer.size}`);
+
+  // Attempt to load ML model
+  const model = await tf.loadLayersModel(mlModelPath);
+  console.log(model.summary());
 }
 
 main();
