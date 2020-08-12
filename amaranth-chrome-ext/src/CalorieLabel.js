3faae9422d94706b75fa4fbf6ad816e6f3210425
Tom Lauerman <tlauerman@google.com>
2020-08-12 15:17:47 +0000

    Remove unnecessary eslint comment

--- amaranth-chrome-ext/src/CalorieLabel.js
+++ amaranth-chrome-ext/src/CalorieLabel.js
@@ -2,7 +2,7 @@
  * Possible calorie labels that can be given to a dish.
  * @enum {number}
  */
-const CalorieLabel = { // eslint-disable-line no-unused-vars
+const CalorieLabel = {
   LOW_CALORIE: 0,
   AVERAGE_CALORIE: 1,
   HIGH_CALORIE: 2,
