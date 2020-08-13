57599a151e27079431950dcd971f2fb4fec5707a
Tom Lauerman <tlauerman@google.com>
2020-08-13 19:01:03 +0000

    Remove test comments and add function to test name

--- amaranth-chrome-ext/test/AmaranthUtil.test.js
+++ amaranth-chrome-ext/test/AmaranthUtil.test.js
@@ -1,37 +1,40 @@
 const AmaranthUtil = require('../src/AmaranthUtil');
 
-// Test AmaranthUtil.removeSpecialCharacters
-test('removes space from "hello there!"', () => {
+test('removeSpecialCharacters: removes space from "hello there!"', () => {
   expect(AmaranthUtil.removeSpecialCharacters('hello there!', ' '))
       .toBe('hellothere!');
 });
 
-test('removes spaces from "  hello     there ! "', () => {
-  expect(AmaranthUtil.removeSpecialCharacters('  hello     there ! ', ' '))
-      .toBe('hellothere!');
-});
+test('removeSpecialCharacters: removes spaces from "  hello     there ! "',
+    () => {
+      expect(AmaranthUtil.removeSpecialCharacters('  hello     there ! ', ' '))
+          .toBe('hellothere!');
+    });
 
-test('removes special characters from "!@#t$#  e#@!*&^()s$#()*t"', () => {
-  expect(AmaranthUtil.removeSpecialCharacters('!@#t$#[(<e#@!*&^()s$#(}]>)*t'))
+test(`removeSpecialCharacters: removes special characters from
+    "!@#t$#[(<e#@!*&^()s$#(}]>)*t"`,
+() => {
+  expect(AmaranthUtil
+      .removeSpecialCharacters('!@#t$#[(<e#@!*&^()s$#(}]>)*t'))
       .toBe('test');
 });
 
-// Test AmaranthUtil.padArray
-test('pads [1,2,3] to [1,2,3,0,0,0,0,0,0,0]', () => {
+test('padArray: pads [1,2,3] to [1,2,3,0,0,0,0,0,0,0]', () => {
   expect(AmaranthUtil.padArray([1, 2, 3], 10, 0))
       .toStrictEqual([1, 2, 3, 0, 0, 0, 0, 0, 0, 0]);
 });
 
-test('pads ["1","2","3"] to ["1","2","3","",""]', () => {
+test('padArray: pads ["1","2","3"] to ["1","2","3","",""]', () => {
   expect(AmaranthUtil.padArray(['1', '2', '3'], 5, ''))
       .toStrictEqual(['1', '2', '3', '', '']);
 });
 
-test('pads [] to [{}, {}, {}]', () => {
+test('padArray: pads [] to [{}, {}, {}]', () => {
   expect(AmaranthUtil.padArray([], 3, {})).toStrictEqual([{}, {}, {}]);
 });
 
-test('does not pad [1,2,3,4,5] which is longer than desired length', () => {
-  expect(AmaranthUtil.padArray([1, 2, 3, 4, 5], 3, 0))
-      .toStrictEqual([1, 2, 3, 4, 5]);
-});
+test('padArray: does not pad [1,2,3,4,5] which is longer than desired length',
+    () => {
+      expect(AmaranthUtil.padArray([1, 2, 3, 4, 5], 3, 0))
+          .toStrictEqual([1, 2, 3, 4, 5]);
+    });
