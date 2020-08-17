fd08ea00e0a1a4d5027d041161d91ea3b8688027
Tom Lauerman <tlauerman@google.com>
2020-08-17 19:23:54 +0000

    Clarify comment

--- amaranth-chrome-ext/src/CalorieLabel.js
+++ amaranth-chrome-ext/src/CalorieLabel.js
@@ -1,7 +1,7 @@
 /**
  * Possible calorie labels that can be given to a dish.
- * @readonly @enum {string} the enum values function as unique identifiers and a
- * sort of `toString()` value
+ * @readonly @enum {string} the enum values function as unique identifiers and
+ * map to a corresponding string value
  */
 const CalorieLabel = {
   LOW_CALORIE: 'Low Calorie',
