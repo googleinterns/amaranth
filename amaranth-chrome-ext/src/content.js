0b7d283188b975215d75058bb1efbc27eeeb2bd0
Tom Lauerman <tlauerman@google.com>
2020-08-17 17:16:12 +0000

    Move constant definitions into main()

--- amaranth-chrome-ext/src/content.js
+++ amaranth-chrome-ext/src/content.js
@@ -1,9 +1,9 @@
-const mlModelPath = chrome.runtime.getURL('assets/model/model.json');
-const tokenizerPath = chrome.runtime.getURL('assets/tokenizer.json');
-const dishNameSelector = '.menuItem-name';
-
 /** The entry point of the Chrome Extension. */
 async function main() {
+  const mlModelPath = chrome.runtime.getURL('assets/model/model.json');
+  const tokenizerPath = chrome.runtime.getURL('assets/tokenizer.json');
+  const dishNameSelector = '.menuItem-name';
+
   // Load tokenizer dictionary
   const response = await fetch(tokenizerPath);
   const tokenizerObj = await response.json();
