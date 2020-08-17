71aab1c6d4597cb82bd4b4c2ca866d03debbbef0
Tom Lauerman <tlauerman@google.com>
2020-08-17 17:23:52 +0000

    Fix indentation

--- amaranth-chrome-ext/src/background.js
+++ amaranth-chrome-ext/src/background.js
@@ -1,20 +1,20 @@
 function injectAmaranth() {
-    chrome.tabs.executeScript(null, {
-        file: 'src/main.js',
-    });
+  chrome.tabs.executeScript(null, {
+    file: 'src/main.js',
+  });
 }
 
 chrome.webNavigation.onCompleted.addListener(injectAmaranth, {
-    // Code should only be injected if on grubhub.com/restaurant/...
-    url: [{pathPrefix: '/restaurant'}],
+  // Code should only be injected if on grubhub.com/restaurant/...
+  url: [{pathPrefix: '/restaurant'}],
 });
 
 chrome.webNavigation.onHistoryStateUpdated.addListener(function() {
-    // When the user navigates to a different restaurant, a new page is shown
-    // for a fraction of a second. The Chrome extension labels this intermediate
-    // page but not the real restaurant page when it loads. This timeout
-    // attempts to avoid labelling the intermediate page.
-    setTimeout(injectAmaranth, 1000);
+  // When the user navigates to a different restaurant, a new page is shown
+  // for a fraction of a second. The Chrome extension labels this intermediate
+  // page but not the real restaurant page when it loads. This timeout
+  // attempts to avoid labelling the intermediate page.
+  setTimeout(injectAmaranth, 1000);
 }, {
-    url: [{pathPrefix: '/restaurant'}],
+  url: [{pathPrefix: '/restaurant'}],
 });
